7fe44925ae2a5f76be48b2a24e275d92ac4819dc
jyxc0000 <123103171+jyxc0000@users.noreply.github.com>
2023-08-07 13:38:37 -0700

    Mental Health

---
+++ mental health/script.js
@@ -0,0 +1,5 @@
+function submit(){
+    let userInput = document.querySelector("#freeform");
+    let message = document.querySelector("#message");
+    message.innerHTML = "Sent, we will respond ASAP!"
+}

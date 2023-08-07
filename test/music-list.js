0b2a9e6772065b76bfcf552a26584a93560460f3
ray060708 <ray060708@gmail.com>
2023-08-07 13:38:49 -0700

    a

---
+++ test/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Chill Lofi",
+        artist: "unknown",
+        img: "lofi",
+        src: "lofi"
+    },
+    {
+        name: "Smooth Jazz",
+        artist: "unknown",
+        img: "jazz",
+        src: "jazz"
+    },
+    {
+        name: "Classical Music",
+        artist: "unknown",
+        img: "classical",
+        src: "classical"
+    },
+    {
+        name: "Nature Sounds",
+        artist: "unknown",
+        img: "nature",
+        src: "nature"
+    },
+    {
+        name: "Soft Rock",
+        artist: "unknown",
+        img: "rock",
+        src: "rock"
+    }
+]
